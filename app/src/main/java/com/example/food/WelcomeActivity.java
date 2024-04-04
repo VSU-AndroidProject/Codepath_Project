a78126829067daa1fc5feda2e07e6c1fd248f76f
SimSledge001 <sledge.simeon@gmail.com>
2024-04-04 10:26:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/food/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void Login(View v){
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+    }
+
+}
+
+

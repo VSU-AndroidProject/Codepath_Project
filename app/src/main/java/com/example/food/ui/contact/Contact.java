cdcdb1ae39d128023cc14641d133818867aded73
michaelebosh <mebosh1@yahoo.com>
2024-04-25 11:57:15 -0400

    Account Page

--- app/src/main/java/com/example/food/ui/contact/Contact.java
+++ app/src/main/java/com/example/food/ui/contact/Contact.java
@@ -3,6 +3,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,9 +14,7 @@ import com.example.food.databinding.FragmentContactBinding;
 import com.example.food.databinding.FragmentHomeBinding;
 
 public class Contact extends Fragment {
-
     private FragmentContactBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
